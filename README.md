eff5fed358beda41965326a53a751e49bbf7bff3
Andreas Christian Olsen <aco@acohimself.com>
2021-12-01 21:41:59 +0100

    Initial template and day01 solution

---
+++ README.md
@@ -0,0 +1,16 @@
+# Advent of code 2021
+Haskell Solutions for Advent of Code 2021 - https://adventofcode.com/
+Template was borrowed from https://github.com/pabloariasal/advent-of-code-2021-haskell
+
+# Usage
+
+## Run specific days
+```
+cabal run aoc21 -- 01 02 12
+```
+
+## Run all days
+
+```
+cabal run aoc21
+```
---
+++ src/Day01.hs
@@ -0,0 +1,21 @@
+module Day01 (solve) where
+
+
+part1 :: [Int] -> Int
+part1 [] = 0
+part1 [_] = 0
+part1 (x:y:xs)
+  | x<y = 1 + part1 (y:xs)
+  | otherwise = part1 (y:xs)
+
+
+part2 :: [Int] -> Int
+part2 (x1:x2:x3:x4:xs)
+  | x1 < x4 = 1 + part2 (x2:x3:x4:xs)
+  | otherwise = part2 (x2:x3:x4:xs)
+part2 _ = 0
+
+
+solve :: String -> IO ()
+solve input = putStrLn "--- Day 01 ---" >> print (part1 $ p input) >> print (part2 $ p input)
+    where p = map read . lines
